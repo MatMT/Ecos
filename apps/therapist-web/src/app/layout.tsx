a16e0a9f559a354d98ff0e0a4e616c85bdbb0615
Martin Carbajal <107637104+EduarM70@users.noreply.github.com>
2026-08-30 15:28:49 -0600

    bugfix(therapist-web): fixing sintax error layout.tsx

--- apps/therapist-web/src/app/layout.tsx
+++ apps/therapist-web/src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Ecos Therapist',
     'Monitorea y analiza datos biomédicos de pacientes para terapeutas con métricas, alertas e insights clínicos.',
+  description: 'Monitorea y analiza datos biomédicos de pacientes para terapeutas con métricas, alertas e insights clínicos.',
   robots: {
     index: false,
     follow: false,
